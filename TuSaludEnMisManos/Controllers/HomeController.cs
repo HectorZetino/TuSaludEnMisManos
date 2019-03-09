3ffae4d1290603f3e41fbdbd98a72ad718673ae2
Hector <h.rzc.zetino@gmail.com>
2019-03-08 18:49:40 -0600

    Carga Archivos CSV

--- TuSaludEnMisManos/Controllers/HomeController.cs
+++ TuSaludEnMisManos/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TuSaludEnMisManos.Models;
+using TuSaludEnMisManos.Controllers;
+using System.IO;
+
 
 namespace TuSaludEnMisManos.Controllers
 {
@@ -10,7 +14,44 @@ namespace TuSaludEnMisManos.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(new List<Medicamento>());
+        }
+        [HttpPost]
+
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            List<Medicamento> medicamentos = new List<Medicamento>();
+            string filePath = string.Empty;
+            if (postedFile != null)
+            {
+                string path = Server.MapPath("~/uploads/");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                filePath = path + Path.GetFileName(postedFile.FileName);
+                string extension = Path.GetExtension(postedFile.FileName);
+                postedFile.SaveAs(filePath);
+
+                string csvData = System.IO.File.ReadAllText(filePath);//ReadAllText
+                foreach (string row in csvData.Split('\n'))
+                {
+                    if (!string.IsNullOrEmpty(row))
+                    {
+                        medicamentos.Add(new Models.Medicamento
+                        {
+                            id_medicamento =Convert.ToInt32(row.Split(',')[0]),
+                            nombre = row.Split(',')[1],
+                            descripcion = row.Split('\"')[1],
+                            casa_productora = row.Split('\"')[3],
+                            precio = row.Split(',')[4],
+                            existencia = Convert.ToInt32( row.Split(',')[5])
+                        });
+                    }
+                }
+
+            }
+            return View(medicamentos);
         }
 
         public ActionResult About()
