7f318dd221ede63af29b98fd321f46e537e4be66
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2019-03-12 09:49:32 -0600

    Regresando a versión anterior

---
+++ LibraryClassTrees/Medicamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TuSaludEnMisManos.Models
+{
+    public class Medicamento<T>
+    {
+        public T Id_medicamento { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public string casa_productora { get; set; }
+        public string precio { get; set; }
+        public int existencia { get; set; }
+    }
+}
---
+++ LibraryClassTrees/TreeState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryClassTrees
+{
+    /// <summary>
+    /// Balance state of an AVLTree
+    /// </summary>
+    public enum TreeState
+    {
+        LeftHeavy,
+        RightHeavy,
+        Balanced
+    }
+}
