5a505591464a987b26d4b4f1493374026696f147
Lester Andrés García Aquino <lagarcia2015url@gmail.com>
2019-03-13 20:35:10 -0600

    Modifying HomeController

--- TuSaludEnMisManos/Controllers/HomeController.cs
+++ TuSaludEnMisManos/Controllers/HomeController.cs
@@ -12,54 +12,13 @@ namespace TuSaludEnMisManos.Controllers
 {
     public class HomeController : Controller
     {
-        private Stream path;
 
-        
-        
+        private Stream path;
 
         public ActionResult Index()
         {
             return View();
         }
-        
-        /*[HttpPost]
-
-public ActionResult Index(HttpPostedFileBase postedFile)
-{
-   List<Medicamento> medicamentos = new List<Medicamento>();
-   string filePath = string.Empty;
-   if (postedFile != null)
-   {
-       string path = Server.MapPath("~/uploads/");
-       if (!Directory.Exists(path))
-       {
-           Directory.CreateDirectory(path);
-       }
-       filePath = path + Path.GetFileName(postedFile.FileName);
-       string extension = Path.GetExtension(postedFile.FileName);
-       postedFile.SaveAs(filePath);
-
-       string csvData = System.IO.File.ReadAllText(filePath);//ReadAllText
-       foreach (string row in csvData.Split('\n'))
-       {
-           if (!string.IsNullOrEmpty(row))
-           {
-               medicamentos.Add(new Models.Medicamento
-               {
-                   id_medicamento =Convert.ToInt32(row.Split(',')[0]),
-                   nombre = row.Split(',')[1],
-                   descripcion = row.Split(',')[1],
-                   casa_productora = row.Split(',')[3],
-                   precio = row.Split(',')[4],
-                   existencia = Convert.ToInt32( row.Split(',')[5])
-               });
-           }
-       }
-
-   }
-   return View(medicamentos);
-}*/
-
         public void FileReader()
         {
             using (StreamReader Lector = new StreamReader(@"C: \URL - Primer ciclo 2019\Estructura de datos\Laboratorios\TuSaludEnMisManos\TuSaludEnMisManos\MOCK_DATA (4)"))
---
+++ TuSaludEnMisManos/Controllers/MedicamentosController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using LibraryClassTrees;
+
+namespace TuSaludEnMisManos.Controllers
+{
+    public class MedicamentosController : Controller
+    {
+        public static AVLTree.AVLTree ArbolMedicamentos = new AVLTree.AVLTree.AVLTree();
+        public static AVLTree.Medicamento[] mostrar;
+        public static List<AVLTree.Medicamento> medicamentos = new List<AVLTree.Medicamento>();
+        public static int a = 0;
+
+        // GET: Medicamentos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Medicamentos/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Medicamentos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Medicamentos/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Medicamentos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Medicamentos/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Medicamentos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Medicamentos/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
